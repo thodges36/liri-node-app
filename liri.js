e163a792ac24c1ed72fa48820dafe5ac7d8b38ff
Ted <ted_hodges@ymail.com>
2018-03-31 10:02:03 -0500

    basic API set up

--- liri.js
+++ liri.js
@@ -1,1 +1,54 @@
+//variables: data keys; fs; twitter; spotify; request
+//functions: find song from spotify; find latest tweets; find movie info; do what it says
+
+//Requesting the npn package
+var request = require("request");
+
 require("dotenv").config();
+
+//Request info from npm website
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
+//Spotify example from npn website
+/* var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: <your spotify client id>,
+  secret: <your spotify client secret>
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+})
+;
+*/
+
+//Twitter example from npm website
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+//OMDB request stuff from ombd website
+//http://www.omdbapi.com/?apikey=[yourkey]&
+
